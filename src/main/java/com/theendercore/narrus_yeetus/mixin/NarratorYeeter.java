e13f81847063c9aa1e6224ae2687ce69518a48f1
theendercore <theendercore@gmail.com>
2023-09-11 15:45:00 +0300

    - version bump - 1.18 crash fix

--- src/main/java/com/theendercore/narrus_yeetus/mixin/NarratorYeeter.java
+++ src/main/java/com/theendercore/narrus_yeetus/mixin/NarratorYeeter.java
@@ -2,19 +2,23 @@ package com.theendercore.narrus_yeetus.mixin;
 
 import com.mojang.text2speech.Narrator;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static com.mojang.text2speech.Narrator.EMPTY;
-import static com.mojang.text2speech.Narrator.LOGGER;
-
 @Mixin(Narrator.class)
 public interface NarratorYeeter {
     @Inject(at = @At("HEAD"), method = "getNarrator", cancellable = true, remap = false)
     private static void getNarrator(@NotNull CallbackInfoReturnable<Narrator> cir) {
-        LOGGER.error("Narrus Yeetus!");
-        cir.setReturnValue(EMPTY);
+        LoggerFactory.getLogger("Narrator").error("Narrus Yeetus!");
+        cir.setReturnValue(new Narrator() {
+            @Override
+            public void say(String msg, boolean interrupt) {}
+            @Override public void clear() {}
+            @Override public boolean active() {return false;}
+            @Override public void destroy() {}
+        });
     }
 }
