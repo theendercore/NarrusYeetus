d4a79285be1cdddda3612d6fb56747ffa8f6b53c
TheEnderCore <theendercore@gmail.com>
2025-04-22 20:51:40 +0300

    - removed error

--- src/main/java/com/theendercore/narrus_yeetus/mixin/NarratorMixin.java
+++ src/main/java/com/theendercore/narrus_yeetus/mixin/NarratorMixin.java
@@ -9,16 +9,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Narrator.class)
-public interface NarratorYeeter {
-    @Inject(at = @At("HEAD"), method = "getNarrator", cancellable = true, remap = false)
+public interface NarratorMixin {
+    @Inject(method = "getNarrator", at = @At("HEAD"), cancellable = true, remap = false)
     private static void getNarrator(@NotNull CallbackInfoReturnable<Narrator> cir) {
-        LoggerFactory.getLogger("Narrator").error("Narrus Yeetus!");
+        LoggerFactory.getLogger("Narrator").info("The Narrator has been disabled by Narrus Yeetus!");
         cir.setReturnValue(new Narrator() {
-            @Override
-            public void say(String msg, boolean interrupt) {}
-            @Override public void clear() {}
-            @Override public boolean active() {return false;}
-            @Override public void destroy() {}
+            @Override public void say(String msg, boolean interrupt) { }
+            @Override public void clear() { }
+            @Override public boolean active() { return false; }
+            @Override public void destroy() { }
         });
     }
 }
